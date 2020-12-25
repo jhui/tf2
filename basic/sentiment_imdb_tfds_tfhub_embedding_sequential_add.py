3e26543b3b094ae46a276864b2bb5fcd91f6e65c
Jonathan Hui <jonathan@jonathanhui.com>
2020-12-25 10:30:37 -0800

    cmt

---
+++ basic/sentiment_imdb_tfds_tfhub_embedding_sequential_add.py
@@ -0,0 +1,41 @@
+# Modified from
+# https://www.tensorflow.org/tutorials/keras/text_classification_with_hub
+
+import os
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+
+# Load compressed models from tensorflow_hub
+os.environ["TFHUB_MODEL_LOAD_FORMAT"] = "COMPRESSED"
+
+# Split the training set into 60% and 40%, so we'll end up with 15,000 examples
+# for training, 10,000 examples for validation and 25,000 examples for testing.
+train_data, validation_data, test_data = tfds.load(
+    name="imdb_reviews",
+    split=('train[:60%]', 'train[60%:]', 'test'),
+    as_supervised=True)
+
+embedding = "https://tfhub.dev/google/nnlm-en-dim50/2"
+hub_layer = hub.KerasLayer(embedding, input_shape=[],
+                           dtype=tf.string, trainable=True)
+
+model = tf.keras.Sequential()
+model.add(hub_layer)
+model.add(tf.keras.layers.Dense(16, activation='relu'))
+model.add(tf.keras.layers.Dense(1))
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(train_data.shuffle(10000).batch(512),
+                    epochs=10,
+                    validation_data=validation_data.batch(512),
+                    verbose=1)
+
+results = model.evaluate(test_data.batch(512), verbose=2)
+
+for name, value in zip(model.metrics_names, results):
+    print("%s: %.3f" % (name, value))
+
