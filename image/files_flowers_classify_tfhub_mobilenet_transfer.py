b91676ea4b133be299daa0047a25f59a76ebee36
Jonathan Hui <jonathan@jonathanhui.com>
2020-12-26 12:26:02 -0800

    Update files_flowers_classify_tfhub_mobilenet_transfer.py

--- image/files_flowers_classify_tfhub_mobilenet_transfer.py
+++ image/files_flowers_classify_tfhub_mobilenet_transfer.py
@@ -6,38 +6,38 @@ import tensorflow_hub as hub
 IMAGE_SHAPE = (224, 224)
 
 # Download pre-trained MobileNet v2
-classifier_model ="https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/4"
+classifier_model = "https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/4"
 classifier = tf.keras.Sequential([
-    hub.KerasLayer(classifier_model, input_shape=IMAGE_SHAPE+(3,))
+    hub.KerasLayer(classifier_model, input_shape=IMAGE_SHAPE + (3,))
 ])
 
 # Download ImageNet label names
 labels_path = tf.keras.utils.get_file('ImageNetLabels.txt',
-    'https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt')
+                                      'https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt')
 imagenet_labels = np.array(open(labels_path).read().splitlines())
 
 # Get flower pictures
 data_root = tf.keras.utils.get_file(
-  'flower_photos',
-  'https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz',
-  untar=True)
+    'flower_photos',
+    'https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz',
+    untar=True)
 
 batch_size = 32
 img_height = 224
 img_width = 224
 
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-  str(data_root),
-  validation_split=0.2,
-  subset="training",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+    str(data_root),
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
 
 # Get the label names of this dataset
 class_names = np.array(train_ds.class_names)
 
-normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
+normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1. / 255)
 train_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
@@ -60,25 +60,27 @@ feature_extractor_layer = hub.KerasLayer(
 num_classes = len(class_names)
 
 model = tf.keras.Sequential([
-  feature_extractor_layer,
-  tf.keras.layers.Dense(num_classes)
+    feature_extractor_layer,
+    tf.keras.layers.Dense(num_classes)
 ])
 
 model.compile(
-  optimizer=tf.keras.optimizers.Adam(),
-  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-  metrics=['acc'])
+    optimizer=tf.keras.optimizers.Adam(),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['acc'])
+
 
 # collect performance for each batch instead of each epoch
 class CollectBatchStats(tf.keras.callbacks.Callback):
-  def __init__(self):
-    self.batch_losses = []
-    self.batch_acc = []
+    def __init__(self):
+        self.batch_losses = []
+        self.batch_acc = []
 
-  def on_train_batch_end(self, batch, logs=None):
-    self.batch_losses.append(logs['loss'])
-    self.batch_acc.append(logs['acc'])
-    self.model.reset_metrics()
+    def on_train_batch_end(self, batch, logs=None):
+        self.batch_losses.append(logs['loss'])
+        self.batch_acc.append(logs['acc'])
+        self.model.reset_metrics()
+
 
 batch_stats_callback = CollectBatchStats()
 
