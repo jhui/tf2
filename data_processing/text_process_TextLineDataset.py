6c6287a150734025876a735ba5b5d8991ecc8fd7
Jonathan Hui <jonathan@jonathanhui.com>
2020-12-25 22:25:52 -0800

    Create text_process_TextLineDataset.py

---
+++ data_processing/text_process_TextLineDataset.py
@@ -0,0 +1,175 @@
+import collections
+import pathlib
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras import losses
+from tensorflow.keras import utils
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+import tensorflow_text as tf_text
+
+DIRECTORY_URL = 'https://storage.googleapis.com/download.tensorflow.org/data/illiad/'
+FILE_NAMES = ['cowper.txt', 'derby.txt', 'butler.txt']
+
+for name in FILE_NAMES:
+    text_dir = utils.get_file(name, origin=DIRECTORY_URL + name)
+
+parent_dir = pathlib.Path(text_dir).parent
+
+
+# Return (data, label)
+def labeler(example, index):
+    return example, tf.cast(index, tf.int64)
+
+
+labeled_data_sets = []
+
+# Iterate through all three files
+for i, file_name in enumerate(FILE_NAMES):
+    lines_dataset = tf.data.TextLineDataset(str(parent_dir / file_name))
+    # Samples in the same file belongs to the same translator i (label)
+    labeled_dataset = lines_dataset.map(lambda ex: labeler(ex, i))
+    labeled_data_sets.append(labeled_dataset)
+
+BUFFER_SIZE = 50000
+BATCH_SIZE = 64
+VALIDATION_SIZE = 5000
+
+# Combine these labeled datasets into a single dataset, and shuffle it.
+all_labeled_data = labeled_data_sets[0]
+for labeled_dataset in labeled_data_sets[1:]:
+    all_labeled_data = all_labeled_data.concatenate(labeled_dataset)
+
+all_labeled_data = all_labeled_data.shuffle(
+    BUFFER_SIZE, reshuffle_each_iteration=False)
+
+# Prepare the dataset for training
+# Instead of using the Keras TextVectorization
+# Use tf.text
+tokenizer = tf_text.UnicodeScriptTokenizer()
+
+
+# tokenize the text
+def tokenize(text, unused_label):
+    lower_case = tf_text.case_fold_utf8(text)
+    return tokenizer.tokenize(lower_case)
+
+
+tokenized_ds = all_labeled_data.map(tokenize)
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+def configure_dataset(dataset):
+    return dataset.cache().prefetch(buffer_size=AUTOTUNE)
+
+# Build a vocabulary by sorting tokens by frequency
+# and keeping the top VOCAB_SIZE tokens.
+tokenized_ds = configure_dataset(tokenized_ds)
+
+# Count word frequency
+vocab_dict = collections.defaultdict(lambda: 0)
+for toks in tokenized_ds.as_numpy_iterator():
+    for tok in toks:
+        vocab_dict[tok] += 1
+
+VOCAB_SIZE = 10000
+
+# Get the top 10000 most frequent words
+vocab = sorted(vocab_dict.items(), key=lambda x: x[1], reverse=True)
+vocab = [token for token, count in vocab]
+vocab = vocab[:VOCAB_SIZE]
+vocab_size = len(vocab)
+print("Vocab size: ", vocab_size)
+print("First five vocab entries:", vocab[:5])
+
+keys = vocab
+values = range(2, len(vocab) + 2)  # reserve 0 for padding, 1 for OOV
+
+# Map a word to a word index
+init = tf.lookup.KeyValueTensorInitializer(
+    keys, values, key_dtype=tf.string, value_dtype=tf.int64)
+
+# String to Id table that assigns out-of-vocabulary keys to hash buckets.
+num_oov_buckets = 1
+vocab_table = tf.lookup.StaticVocabularyTable(init, num_oov_buckets)
+
+
+def preprocess_text(text, label):
+    standardized = tf_text.case_fold_utf8(text)
+    tokenized = tokenizer.tokenize(standardized)
+    vectorized = vocab_table.lookup(tokenized)
+    return vectorized, label
+
+
+all_encoded_data = all_labeled_data.map(preprocess_text)
+
+train_data = all_encoded_data.skip(VALIDATION_SIZE).shuffle(BUFFER_SIZE)
+validation_data = all_encoded_data.take(VALIDATION_SIZE)
+
+train_data = train_data.padded_batch(BATCH_SIZE)
+validation_data = validation_data.padded_batch(BATCH_SIZE)
+
+vocab_size += 2
+
+
+def create_model(vocab_size, num_labels):
+    model = tf.keras.Sequential([
+        layers.Embedding(vocab_size, 64, mask_zero=True),
+        layers.Conv1D(64, 5, padding="valid", activation="relu", strides=2),
+        layers.GlobalMaxPooling1D(),
+        layers.Dense(num_labels)
+    ])
+    return model
+
+
+train_data = configure_dataset(train_data)
+validation_data = configure_dataset(validation_data)
+
+model = create_model(vocab_size=vocab_size, num_labels=3)
+model.compile(
+    optimizer='adam',
+    loss=losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'])
+history = model.fit(train_data, validation_data=validation_data, epochs=3)
+
+loss, accuracy = model.evaluate(validation_data)
+
+print("Loss: ", loss)
+print("Accuracy: {:2.2%}".format(accuracy))
+
+# Export the model #
+MAX_SEQUENCE_LENGTH = 250
+
+preprocess_layer = TextVectorization(
+    max_tokens=vocab_size,
+    standardize=tf_text.case_fold_utf8,
+    split=tokenizer.tokenize,
+    output_mode='int',
+    output_sequence_length=MAX_SEQUENCE_LENGTH)
+preprocess_layer.set_vocabulary(vocab)
+
+export_model = tf.keras.Sequential(
+    [preprocess_layer, model,
+     layers.Activation('sigmoid')])
+
+export_model.compile(
+    loss=losses.SparseCategoricalCrossentropy(from_logits=False),
+    optimizer='adam',
+    metrics=['accuracy'])
+
+# Create a test dataset of raw strings
+test_ds = all_labeled_data.take(VALIDATION_SIZE).batch(BATCH_SIZE)
+test_ds = configure_dataset(test_ds)
+loss, accuracy = export_model.evaluate(test_ds)
+print("Loss: ", loss)
+print("Accuracy: {:2.2%}".format(accuracy))
+
+inputs = [
+    "Join'd to th' Ionians with their flowing robes,",  # Label: 1
+    "the allies, and his armour flashed about him so that he seemed to all",  # Label: 2
+    "And with loud clangor of his arms he fell.",  # Label: 0
+]
+predicted_scores = export_model.predict(inputs)
+predicted_labels = tf.argmax(predicted_scores, axis=1)
+for input, label in zip(inputs, predicted_labels):
+    print("Question: ", input)
+    print("Predicted label: ", label.numpy())
