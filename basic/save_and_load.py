4ffef944f70b86f02cf2d6a4b06d16eea229e85d
Jonathan Hui <jonathan@jonathanhui.com>
2020-12-25 19:01:43 -0800

    Update save_and_load.py

--- basic/save_and_load.py
+++ basic/save_and_load.py
@@ -1,5 +1,7 @@
-import os
+# Modified from
+# https://www.tensorflow.org/tutorials/keras/save_and_load
 
+import os
 import tensorflow as tf
 from tensorflow import keras
 
@@ -10,4 +12,166 @@ test_labels = test_labels[:1000]
 
 train_images = train_images[:1000].reshape(-1, 28 * 28) / 255.0
 test_images = test_images[:1000].reshape(-1, 28 * 28) / 255.0
+
+
+# Define a simple sequential model
+def create_model():
+    model = tf.keras.models.Sequential([
+        keras.layers.Dense(512, activation='relu', input_shape=(784,)),
+        keras.layers.Dropout(0.2),
+        keras.layers.Dense(10)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=[tf.metrics.SparseCategoricalAccuracy()])
+
+    return model
+
+
+# Create a basic model instance
+model = create_model()
+
+checkpoint_path = "training_1/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+
+# Train the model with the new callback
+model.fit(train_images,
+          train_labels,
+          epochs=15,
+          validation_data=(test_images, test_labels),
+          callbacks=[cp_callback])  # Pass callback to training
+
+# Create a basic model instance
+model = create_model()
+
+# Loads the weights
+model.load_weights(checkpoint_path)
+
+# Re-evaluate the model
+loss, acc = model.evaluate(test_images, test_labels, verbose=2)
+print("Restored model, accuracy: {:5.2f}%".format(100 * acc))
+
+# Checkpoint Options #
+
+# Include the epoch in the file name (uses `str.format`)
+checkpoint_path = "training_2/cp-{epoch:04d}.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+batch_size = 32
+
+# Create a callback that saves the model's weights every 5 epochs
+cp_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_path,
+    verbose=1,
+    save_weights_only=True,
+    save_freq=5 * batch_size)
+
+# Create a new model instance
+model = create_model()
+
+# Train the model with the new callback
+model.fit(train_images,
+          train_labels,
+          epochs=50,
+          callbacks=[cp_callback],
+          validation_data=(test_images, test_labels),
+          verbose=0)
+
+# Load latest checkpoint #
+
+latest = tf.train.latest_checkpoint(checkpoint_dir)
+model = create_model()
+
+# Load the previously saved weights
+model.load_weights(latest)
+
+# Save weights manually #
+# Save the weights
+model.save_weights('./checkpoints/my_checkpoint')
+
+# Create a new model instance
+model = create_model()
+
+# Restore the weights
+model.load_weights('./checkpoints/my_checkpoint')
+
+# Save the whole model #
+model.save('saved_model/my_model')
+
+new_model = tf.keras.models.load_model('saved_model/my_model')
+
+
+# CheckpointManager #
+class Net(tf.keras.Model):
+    """A simple linear model."""
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.l1 = tf.keras.layers.Dense(5)
+
+    def call(self, x):
+        return self.l1(x)
+
+
+net = Net()
+
+
+def toy_dataset():
+    inputs = tf.range(10.)[:, None]
+    labels = inputs * 5. + tf.range(5.)[None, :]
+    return tf.data.Dataset.from_tensor_slices(
+        dict(x=inputs, y=labels)).repeat().batch(2)
+
+
+def train_step(net, example, optimizer):
+    """Trains `net` on `example` using `optimizer`."""
+    with tf.GradientTape() as tape:
+        output = net(example['x'])
+        loss = tf.reduce_mean(tf.abs(output - example['y']))
+    variables = net.trainable_variables
+    gradients = tape.gradient(loss, variables)
+    optimizer.apply_gradients(zip(gradients, variables))
+    return loss
+
+
+opt = tf.keras.optimizers.Adam(0.1)
+dataset = toy_dataset()
+iterator = iter(dataset)
+ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=opt, net=net, iterator=iterator)
+manager = tf.train.CheckpointManager(ckpt, './tf_ckpts', max_to_keep=3)
+
+
+def train_and_checkpoint(net, manager):
+    ckpt.restore(manager.latest_checkpoint)
+    if manager.latest_checkpoint:
+        print("Restored from {}".format(manager.latest_checkpoint))
+    else:
+        print("Initializing from scratch.")
+
+    for _ in range(50):
+        example = next(iterator)
+        loss = train_step(net, example, opt)
+        ckpt.step.assign_add(1)
+        if int(ckpt.step) % 10 == 0:
+            save_path = manager.save()
+            print("Saved checkpoint for step {}: {}".format(int(ckpt.step), save_path))
+            print("loss {:1.2f}".format(loss.numpy()))
+
+
+train_and_checkpoint(net, manager)
+
+opt = tf.keras.optimizers.Adam(0.1)
+net = Net()
+dataset = toy_dataset()
+iterator = iter(dataset)
+ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=opt, net=net, iterator=iterator)
+manager = tf.train.CheckpointManager(ckpt, './tf_ckpts', max_to_keep=3)
+
+train_and_checkpoint(net, manager)
 
