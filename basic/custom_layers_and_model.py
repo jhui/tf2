035deb4701b0aa1b06d3de214252aaf11efd293a
Jonathan Hui <jonathan@jonathanhui.com>
2020-12-25 23:25:56 -0800

    Create custom_layers_and_model.py

---
+++ basic/custom_layers_and_model.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+
+
+class MyDenseLayer(tf.keras.layers.Layer):
+    def __init__(self, num_outputs):
+        super(MyDenseLayer, self).__init__()
+        self.num_outputs = num_outputs
+
+    def build(self, input_shape):
+        self.kernel = self.add_weight("kernel",
+                                      shape=[int(input_shape[-1]),
+                                             self.num_outputs])
+
+    def call(self, input):
+        return tf.matmul(input, self.kernel)
+
+
+layer = MyDenseLayer(10)
+
+_ = layer(tf.zeros([10, 5])) # Calling the layer `.builds` it.
+
+class ResnetIdentityBlock(tf.keras.Model):
+  def __init__(self, kernel_size, filters):
+    super(ResnetIdentityBlock, self).__init__(name='')
+    filters1, filters2, filters3 = filters
+
+    self.conv2a = tf.keras.layers.Conv2D(filters1, (1, 1))
+    self.bn2a = tf.keras.layers.BatchNormalization()
+
+    self.conv2b = tf.keras.layers.Conv2D(filters2, kernel_size, padding='same')
+    self.bn2b = tf.keras.layers.BatchNormalization()
+
+    self.conv2c = tf.keras.layers.Conv2D(filters3, (1, 1))
+    self.bn2c = tf.keras.layers.BatchNormalization()
+
+  def call(self, input_tensor, training=False):
+    x = self.conv2a(input_tensor)
+    x = self.bn2a(x, training=training)
+    x = tf.nn.relu(x)
+
+    x = self.conv2b(x)
+    x = self.bn2b(x, training=training)
+    x = tf.nn.relu(x)
+
+    x = self.conv2c(x)
+    x = self.bn2c(x, training=training)
+
+    x += input_tensor
+    return tf.nn.relu(x)
+
+
+block = ResnetIdentityBlock(1, [1, 2, 3])
+
+_ = block(tf.zeros([1, 2, 3, 3]))
+
